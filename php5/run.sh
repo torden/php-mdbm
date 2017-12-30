50aca7a78a270e143cb73e43e32fa58446f23319
Torden <torden@myself.com>
2017-12-31 02:42:58 +0900

    update

--- php5/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#/app/php5/bin/phpize --clean
-#/app/php5/bin/phpize
-make clean
-./configure --with-php-config=/app/php5/bin/php-config --with-mdbm=/usr/local/mdbm/
-make 
-/app/php5/bin/php -d extension=./modules/mdbm.so z.php
-gdb --args /app/php5/bin/php -d extension=./modules/mdbm.so z.php
