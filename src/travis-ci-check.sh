c6377c7ef9ad103c4f167b68f977cf0f67966a29
Torden Cho <ioemen@gmail.com>
2018-01-02 19:23:34 +0900

    update

--- src/travis-ci-check.sh
+++ src/travis-ci-check.sh
@@ -2,14 +2,14 @@
 CNT=$(find ./tests/ -name "*.mem" | wc -l)
 if [ $CNT -gt 0 ]; then
     echo "FAIL : $CNT";
+    ls -la ./tests/
     cat ./tests/*.mem
     cat ./tests/*.log
 
-OREFILE=$(find . -maxdepth 1 -name "*core*" | head -n 1)
+OREFILE=$(find . -name "*core*" | head -n 1)
 if [[ -f "$COREFILE" ]]; then 
     gdb -c "$COREFILE" php -ex "bt" -ex "set pagination 0" -batch;
 fi
-
 
     exit 1;
 else
